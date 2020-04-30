30309dcc9499e11f4f7ebb90d517a646a88dbfbd
owncreed93 <christiantarifer@gmail.com>
2020-04-30 01:39:50 -0500

    repaso variables

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+'use strict'
+
+console.log("Hola JS")
+
+// * DISPLAYS A WARN IN THE CONSOLE
+console.warn('Esto es una advertencia bandido!!!')
+
+// * DISPLAYS AN ERROR IN THE CONSOLE
+console.error('Se prendió esta shit!!!')
+
+// * SET THE TIMER
+console.time('Hola')
+console.log('Papu Mujeriego')
+console.log('Papu hacker')
+
+// * STOPS THE TIMER
+console.timeEnd('Hola')
+
+var nombre
+
+if (nombre === undefined ){
+    console.log("*********************************")
+    console.log(typeof(nombre))
+    console.log('Papu es camperaso y apoya a los chinos xd')
+    console.log("*********************************")
+}
+
+const cliente = 'Juan'
+
+console.log(cliente)
+
+const divMensaje = document.getElementById('mensaje')
